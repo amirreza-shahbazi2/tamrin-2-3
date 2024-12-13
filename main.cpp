971b0c97f88e603a1706c0447ceb6d58a0815878
amirreza-shahbazi2 <shbazy02@gmail.com>
2024-12-13 06:50:00 -0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,30 +4,27 @@ using namespace std;
 
 int main()
 {
-float a,b,c,gam,zarb,taghsim,tagrigh,khroog ;
-while (a<=4)
-{cout<< "gam=1  tagrigh=2  zarb=3 taghsim=4 khroog=5  ";
-  cin >>a ;
-  if (a==5)
-    break ;
-  else
-  cout << "number:" ;
-  cin >> b ;
-  cout << "number:" ;
-  cin >> c ;
-  if (a==1 )
-   { gam =b+c ;
-    cout <<gam<<"\n" ; }
-  else if (a==2)
-   { tagrigh=b-c ;
-    cout << tagrigh <<"\n"; }
-  else if (a==3)
-  { zarb=b*c ;
-    cout << zarb <<"\n"; }
-  else
-  { taghsim=b/c ;
-    cout<< taghsim<<"\n"; }
-}
-return 0 ;
+    int a,b, ma ,kmm ;
+    cout << "enter a number:" ;
+    cin >> a ;
+    cout <<"enter a number:" ;
+    cin>> b ;
 
+    if(a<b)
+    {ma = b;}
+      else
+        {ma=a;}
+
+    while(true)
+{
+    if (ma%a==0&&ma%b==0)
+    {
+      kmm= ma;
+      break; }
+     else
+         ma++;
+
+}
+    cout <<kmm;
+    return 0;
 }
